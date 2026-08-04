4045eda7b0d3e1e1d4303cc30cdc45e7c6637351
YiberRivas <yiberrenteria16@gmail.com>
2026-08-04 16:16:40 -0500

    pagina + app conectada al backend hacer tes para correciones

--- Frontend/src/config/env.js
+++ Frontend/src/config/env.js
@@ -1,8 +1,8 @@
 import Constants from 'expo-constants';
 
 const ENV = {
-  API_BASE_URL: Constants.expoConfig?.extra?.apiBaseUrl || 'http://10.164.87.181:8000',
-  WS_BASE_URL: Constants.expoConfig?.extra?.wsBaseUrl || 'ws://10.164.87.181:8000',
+  API_BASE_URL: Constants.expoConfig?.extra?.apiBaseUrl || 'http://1172.16.30.129:8000',
+  WS_BASE_URL: Constants.expoConfig?.extra?.wsBaseUrl || 'ws://172.16.30.129:8000',
 };
 
 export default ENV;
